5b4570cb337dd132aba2e2b2b3bb7f903a7ce9e3
Quobb <kueprotocol11@gmail.com>
2022-10-07 12:37:31 +0000

    my first commit

--- function.js
+++ function.js
@@ -1,10 +1,40 @@
-function monitorCount(rows, columns) {
+funct = (rows, columns) => { 
     return rows * columns;
   };
   
-  function costOfMonitors(rows, columns) {
+  const champ = (rows, columns)=> {
     return monitorCount(rows, columns)*200;
   };
-  const totalCost =costOfMonitors(5,4);
+  
+  const itors = (rows, columns) => {
+    return monitorCount(rows, columns)*200;
+  };
+  const totalCost = champ (5,4);
   
   console.log(totalCost)
+
+  //new function
+
+  const plantNeedsWater = (day) =>{
+
+    if (day === 'Wednesday')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  };
+  
+  console.log(plantNeedsWater('Tuesday'));
+
+  //third function
+    const satellite = 'The Moon';
+    const galaxy = 'The Milky Way';
+    const stars ='North Star'
+    const callMyNightSky =()=>{
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky())
+  
